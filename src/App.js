0643b5579cd7546b7e21e903bdd4f3c0cd1efce5
Lapinas <mrlapinas99@gmail.com>
2022-01-25 13:07:43 +0000

    Added Hooks & Request & Display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import React, { useState } from 'react';
 import './index.css';
 
 const App = () => {
+
+  // URL: https://random.dog/woof.json
+
+  // Method
+  const [image, setImage] = useState({});
+
+  // Function
+  const handleFetch = async () => {
+
+    // Request
+    const res = await fetch("https://random.dog/woof.json");
+    // console.log(res);
+
+    // JSON
+    const data = await res.json();
+    // console.log(data);
+
+    // Display it
+    // console.log(data.url);
+
+    // Apply to hook
+    setImage(data);
+  };
+
+
+
+
+
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h1 className='theTitle'>Funny OR Cute Dog Images.</h1>
+      <button onClick={handleFetch}>GIVE ME IMAGE</button>
+      <p>Bytes for this Image: {image.fileSizeBytes}</p>
+      <img src={image.url} alt="" />
     </div>
   );
 }
